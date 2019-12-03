9bb7d60b95a1eeada0ae0c806960a6d1339a82a5
Fred Ulrich <9284621+fulrich@users.noreply.github.com>
2019-12-03 14:18:58 -0500

    Completed Day 2 - Problem 2

--- src/test/scala/ProblemRunner.scala
+++ src/test/scala/ProblemRunner.scala
@@ -1,5 +1,5 @@
 import day1.{FuelCounterUpper, ModuleFueler, ShipModules}
-import day2.{IntcodeComputer, IntcodePrograms}
+import day2.{IntcodeComputer, IntcodePrograms, NounVerbFinder}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
@@ -8,6 +8,7 @@ class ProblemRunner extends AnyFunSuite with Matchers {
     val modules = ShipModules.modules.get
     val fueledModules = modules.map(module => module.copy(fuel = ModuleFueler.fuelForMass(module.mass)))
     val requiredFuel = FuelCounterUpper.totalFuelFor(fueledModules)
+    requiredFuel shouldBe 3497399
 
     println(s"Day 1 - Problem 1 Answer: ${requiredFuel}")
   }
@@ -16,6 +17,7 @@ class ProblemRunner extends AnyFunSuite with Matchers {
     val modules = ShipModules.modules.get
     val fueledModules = ModuleFueler.fuelUpModules(modules)
     val requiredFuel = FuelCounterUpper.totalFuelFor(fueledModules)
+    requiredFuel shouldBe 5243207
 
     println(s"Day 1 - Problem 2 Answer: ${requiredFuel}")
   }
@@ -25,7 +27,15 @@ class ProblemRunner extends AnyFunSuite with Matchers {
     val gravityAssistProgram = IntcodePrograms.gravityAssist.get
     val gravityAssistLastState = gravityAssistProgram.set(1, 12).set(2, 2)
     val gravityAssistOutput = IntcodeComputer.run(gravityAssistLastState).address(0)
+    gravityAssistOutput shouldBe 5098658
 
     println(s"Day 2 - Problem 1 Answer: ${gravityAssistOutput}")
+  }
+
+  test("Day 2 - Program 2: Fixing the Gravity Assist Program") {
+    val foundValue = NounVerbFinder.find()
+    foundValue shouldBe 5064
+
+    println(s"Day 2 - Problem 1 Answer: ${NounVerbFinder.find()}")
   }
 }
