be16479f3a41935cca46ed642b6a1add1b4782aa
Fred Ulrich <9284621+fulrich@users.noreply.github.com>
2019-12-04 14:26:57 -0500

    Minor Day 4 cleanup

--- src/main/scala/day4/PasswordDeterminer.scala
+++ src/main/scala/day4/PasswordDeterminer.scala
@@ -1,7 +1,9 @@
 package day4
 
+
 object PasswordDeterminer {
   val PasswordRange: Range = 264793 to 803935
+  val FillerDigit: Byte = -1
 
 
   def validPasswords(onlyTwoAdjacent: Boolean = false): Seq[Int] =
@@ -15,28 +17,19 @@ object PasswordDeterminer {
 
     hasAdjacentDigits(passwordByteArray) &&
       hasNoDecreasingDigits(passwordByteArray) &&
-      (if (onlyTwoAdjacent) adjacentIsOnlyTwo(passwordByteArray) else true)
+      (if (onlyTwoAdjacent) hasOnlyTwoAdjacentDigits(passwordByteArray) else true)
   }
 
 
-  def hasAdjacentDigits(password: Seq[Byte]): Boolean = slidingWindow(password).foldLeft(false) {
-    case (hasAdjacent, Seq(first, second)) => hasAdjacent || first == second
+  def hasAdjacentDigits(digits: Seq[Byte]): Boolean = digits.sliding(2).exists {
+    case Seq(first, second) => first == second
   }
 
-  def hasNoDecreasingDigits(password: Seq[Byte]): Boolean = slidingWindow(password).foldLeft(true) {
-    case (hasDecreasing, Seq(first, second)) => hasDecreasing && first <= second
+  def hasNoDecreasingDigits(digits: Seq[Byte]): Boolean = digits.sliding(2).forall {
+    case Seq(first, second) => first <= second
   }
 
-  def slidingWindow(password: Seq[Byte]): Iterator[Seq[Byte]] = password.sliding(2)
-
-
-  def adjacentIsOnlyTwo(password: Seq[Byte]): Boolean =
-    innerAdjacentIsOnlyTwo(password) ||
-      (password.take(2).distinct.length == 1 && password.take(3).distinct.length != 1) ||
-      (password.takeRight(2).distinct.length == 1 && password.takeRight(3).distinct.length != 1)
-
-  def innerAdjacentIsOnlyTwo(password: Seq[Byte]): Boolean = password.sliding(4).foldLeft(false) {
-    case (_, Seq(a, b, c, d)) if b == c && b != a && b != d => true
-    case (includesDuplicate, _) => includesDuplicate
+  def hasOnlyTwoAdjacentDigits(digits: Seq[Byte]): Boolean = (FillerDigit +: digits :+ FillerDigit).sliding(4).exists {
+    case Seq(a, b, c, d) => a != b && b == c && c != d
   }
 }
